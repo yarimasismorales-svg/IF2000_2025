8b6cb1114142be525d680e5c24a8cb689d0bd351
Yari <yarimasismorales@gmail.com>
2025-08-14 11:29:26 -0600

    Mi primer commit en Java

---
+++ IF2000_introduccionprogra/IF2000_introduccionProgra.java
@@ -0,0 +1,27 @@
+package IF2000_introduccionprogra;
+
+public class IF2000_introduccionProgra {
+
+
+    public static void main(String[] args) {
+     
+        String name = "Lion";
+        int age = 40;
+
+        System.out.println("Hello world");
+        System.out.println("name:" + name);
+        System.out.println("age:" + age);
+    /*tipos de datos numericos
+    float double , long , short, numeric, money, decimal
+    Manejo de fechas:
+    Date
+    Cdenas de caracteres:
+    String
+    objectos: 
+    object
+
+    Data
+    */
+    }
+    
+}
