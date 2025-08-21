462cc9f5796615e075bce31ad0b106e1a7f96793
Yari <yarimasismorales@gmail.com>
2025-08-21 10:37:37 -0600

    Ejercicios_Semana2_PracticaEnClase

---
+++ logic/Ejecicios_Semana2_PracticaEnClase.java
@@ -0,0 +1,19 @@
+public class Ejecicios_Semana2_PracticaEnClase {
+
+    public Ejecicios_Semana2_PracticaEnClase(){
+
+    }
+    public void Quiz2_A_primos(int n){
+    for(int i = 1 ;i <= n; i++){
+
+    
+    for (int j = 1; j <=(n-i)-1; j++){
+    }
+    for (int k = 1;k <=(2*i-1); k++){
+        System.out.porintln(" ")
+
+    }
+
+}
+}
+}
