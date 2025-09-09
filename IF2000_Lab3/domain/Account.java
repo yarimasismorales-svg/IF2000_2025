ef56ebff7f21242faafbc5f1925310e19ca4b970
yarimasismorales-svg <yarimasismorales@gmail.com>
2025-09-09 10:27:09 -0600

    lab3

---
+++ IF2000_Lab3/domain/Account.java
@@ -0,0 +1,56 @@
+package domain;
+
+public abstract class Account {
+    // Atributos
+    private String accountNumber;
+    private double balance;
+    private Person client;
+
+    // Constructor
+    public Account() {
+       
+    }
+    public Account(String accountNumber, double balance, Person client) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+        this.client = client;
+    }
+    // Getters y Setters
+    public String getAccountNumber() {
+        return accountNumber;
+    }   
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+    public double getBalance() {
+        return balance;
+    }
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+    public Person getClient() {
+        return client;
+    }
+    public void setClient(Person client){
+        this.client = client;
+    }
+
+public abstract void deposit(double amount);
+public abstract void withdraw(double amount);
+public abstract double interestCalculation();
+
+@Override
+public String toString(){
+    String result = "\nBANK ACCOUNT INFORMATIONM/n"+
+    "Account Number: " + this.getAccountNumber() + 
+    "/n"
+     +
+    "Balance: " + this.getBalance()
+    +  "\n"
+    +"\nAccount owner: " + this.getClient().toString();
+
+    return result;
+}
+}
+
+
---
+++ IF2000_Lab3/domain/Person.java
@@ -0,0 +1,59 @@
+package domain;
+
+public class Person {
+    //Atributes
+    private String name;
+    private  String LastName;
+    private  String id;
+    private String phone;
+    private int age;
+
+    public Person(String name,String LastName, String id, String phone,int age){
+    this.name = name;
+    this.LastName = LastName;
+    this.id = id;
+    this.phone = phone;
+    this.age = age;
+    }
+    
+
+    public Person(){}
+   public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getLastname() {
+        return LastName;
+    }
+    public void setLastname(String lastname) {
+        LastName = lastname;
+    }
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    public int getAge() {
+        return age;
+    }   
+    public void setAge(int age) {
+        this.age = age;
+    }
+    @Override
+    public String toString() {
+        return "Person [name=" 
+        + name + ", Lastname=" + LastName + ", id=" + id + ", phone=" + phone + ", age=" + age
+            +"]";
+}
+
+    
+}
