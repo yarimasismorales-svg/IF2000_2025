1496be92d49fb013da7b0a5b5c12672f4093a2d7
yarimasismorales-svg <yarimasismorales@gmail.com>
2025-09-17 23:46:42 -0600

    Lab3

--- IF2000_introduccionprogra/IF2000_introduccionProgra.java
+++ IF2000_introduccionprogra/IF2000_introduccionProgra.java
@@ -24,14 +24,31 @@ public class IF2000_introduccionProgra {
     */
     Ejecicios_Semana2_PracticaEnClase ej;
     ej = new Ejecicios_Semana2_PracticaEnClase();
-    //ej.Quiz2_A_primos(5);
-    //ej.EjercicioA(5);
-    //ej.EjercioB(12);
-    //ej.EjercicoC(10);
+    ej.Quiz2_A_primos(5);
+    ej.EjercicioA(5);
+    ej.EjercioB(12);
+    ej.EjercicoC(10);
     //ej.EjercicioD();
     //ej.ejercicioE();
     //ej.ejercicioF();
-    ej.Quiz_3_cuadrado(5);
+    //ej.Quiz_3_cuadrado(5);
+
+    Lab_1 lab;
+    lab = new Lab_1();
+
+    //lab.Ejercicio_1();
+    lab.Ejercicio_2();
+    //lab.Ejercicio_3();
+    lab.Ejercicio_4();
+    lab.Ejercicio_5();
+    lab.Ejercicio_6();
+    lab.Ejercicio_7();
+    lab.Ejercicio_8();
+    lab.Ejercicio_9();
+    lab.Ejercicio_10();
+    lab.Ejercicio_11();
+    lab.Ejercicio_12();
+
     }
     
 }
